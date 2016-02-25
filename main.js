ad164002a70fcfed3fc4697d17d5b4a5a6ae9963
Justin Smith <justin_tsmith_14@yahoo.com>
2016-02-25 15:33:34 -0500

    final draft

--- main.js
+++ main.js
@@ -1,31 +1,23 @@
-
-
 $('#vote').on('submit', function() {
-    var baseUrl = 'https://docs.google.com/forms/d/1EfMbgiUZNYXykAkeelbwlckQucAkfIxzThHOnj7jH5M/formResponse?';
-    var submitRef = '&submit=Submit';
-    var firstName = encodeURIComponent($('#firstName').val());
-    var firstNameId = 'entry.2022028264';
-    var lastName = encodeURIComponent($('#lastName').val());
-    var lastNameId = 'entry.35492775';
+    var firstNameValue = $('#firstName').val();
+    var lastNameValue = $('#lastName').val();
 
-    var submitUrl = (baseUrl + firstNameId + '=' + firstName + '&' + lastNameId + '=' + lastName + submitRef);
-    $(this)[0].action = submitUrl;
-    var url = $(location).attr('protocol');
-    $(location).attr('href', url + 'share.html');
+    if (firstNameValue.length < 1 || !firstNameValue.trim() || lastNameValue.length < 1 || !lastNameValue.trim()) {
+        alert('Please complete all fields');
+        console.log('Please complete all fields');
+    } else {
+        var baseUrl = 'https://docs.google.com/forms/d/1EfMbgiUZNYXykAkeelbwlckQucAkfIxzThHOnj7jH5M/formResponse?';
+        var submitRef = '&submit=Submit';
+        var firstName = encodeURIComponent(firstNameValue);
+        var firstNameId = 'entry.2022028264';
+        var lastName = encodeURIComponent(lastNameValue);
+        var lastNameId = 'entry.35492775';
+
+        var submitUrl = (baseUrl + firstNameId + '=' + firstName + '&' + lastNameId + '=' + lastName + submitRef);
+        $(this)[0].action = submitUrl;
+        var url = $(location).attr('protocol');
+        $(location).attr('href', url + 'share.html');
+    }
+
 });
 
-function validateFirst() {
-    var x = document.forms["voteForStu"]["firstName"].value;
-    if (x == " ") {
-        alert("Please complete that name fields");
-        return false;
-    }
-}
-
-function validateLast() {
-    var x = document.forms["voteForStu"]["lastName"].value;
-    if (x == " ") {
-        alert("Please complete that name fields");
-        return false;
-    }
-}
